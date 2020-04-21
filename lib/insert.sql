fdac0d884cae25cb302632c64938f35c2480b394
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-21 17:01:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Mr. Chocolate", 18, "Male", "brown", "Sleepy" );
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Rowdy", 16, "Male", "brown", "Hyper" );
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Tabitha", 4, "Female", "brown", "Calm" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Sergeant Brown", 322, "Male", "brown", "Grumpy" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Melissa", 20, "Female", "brown", "Passive" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Grinch", 52, "Male", "brown", "Grumpy" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Wendy", 18, "Female", "brown", "Affectionate" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES (nil, 18, "Male", "brown", nil );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Sergeant Brown", 322, "Male", "brown", "Grumpy" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Melissa", 20, "Female", "brown", "Passive" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Grinch", 52, "Male", "brown", "Grumpy" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Wendy", 18, "Female", "brown", "Affectionate" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES (nil, 18, "Male", "brown", nil );
