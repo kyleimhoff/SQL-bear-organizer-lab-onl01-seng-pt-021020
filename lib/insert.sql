10572018272be75640077c8f8e1885be7925c314
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-21 16:51:07 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Mr. Chocolate, 18, ")
+INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Mr. Chocolate", 18, "Male", "brown", "Sleepy" )
