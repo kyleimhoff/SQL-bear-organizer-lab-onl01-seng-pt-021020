59a85b173e4fbcec5e84f49a3bed2b3f318beebb
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-04-21 17:01:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Mr. Chocolate", 18, "Male", "brown", "Sleepy" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Rowdy", 16, "Male", "brown", "Hyper" );
-INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Tabitha", 4, "Female", "brown", "Calm" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Mr. Chocolate", 18, "Male", "brown", "Sleepy" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Rowdy", 16, "Male", "brown", "Hyper" );
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ("Tabitha", 4, "Female", "brown", "Calm" );
 INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Sergeant Brown", 322, "Male", "brown", "Grumpy" );
 INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Melissa", 20, "Female", "brown", "Passive" );
 INSERT INTO bears (name, age, gender, color, temperment) VALUES ("Grinch", 52, "Male", "brown", "Grumpy" );
